02e29eb644e0c79f006af64c2eacb4bbdd1973bc
Fernanda <fernanda@PC-Fernanda>
2017-07-24 21:09:30 -0300

    Lerning HTML and CSS

---
+++ Ruby/hello.rb
@@ -0,0 +1,11 @@
+#nome = gets.chomp
+#idade = 20
+#puts "A idade de #{nome} é #{idade}"
+# puts "Digite um numero"
+# num = gets.chomp.to_i
+# puts "O dobro é #{num * 2}"
+
+puts 'Qual o seu peso:'
+peso = gets.chomp.to_f
+puts "O seu peso é #{peso}"
+
