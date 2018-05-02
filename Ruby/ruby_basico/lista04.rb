c6b71dacda6cff468e0ffb37dac1db2c4865574f
fernanda <fernandakagami@gmail.com>
2018-05-02 09:30:37 -0300

    Exercícios de Ruby

--- Ruby/ruby_basico/lista04.rb
+++ Ruby/ruby_basico/lista04.rb
@@ -1,4 +1,8 @@
-#01_1
+=begin 01_1 Leia o número de habitantes de uma determinada cidade. O usuário digitará no
+mínimo 1. Para cada habitante, leia o nome e o consumo de energia elétrica. Imprima o nome o
+consumo do habitante que mais e que menos consome. Também imprima a média de consumo da
+cidade.
+=end
 cidade = {}
 
 consumo_cidade = 0
@@ -37,7 +41,9 @@ puts "O habitante que mais consumiu energia foi #{maior_consumo_nome} com consum
 puts "O habitante que menos consumiu energia foi #{menor_consumo_nome} com consumo de #{menor_consumo_energia}."
 puts "A média de consumo da cidade foi #{consumo_cidade/habitante}."
 
-#02_1
+=begin 02_1 Leia uma string. Descubra quais são os dois caracteres que mais se repetem. Imprima
+a string sem esses caracteres.
+=end
 string = "hellooo"
 letra = string.split(//)
 
@@ -71,7 +77,10 @@ end
 puts string = letra.join
 puts hashLetra
 
-#03_1
+=begin 03_1 Leia uma string de 16 caracteres. Transforme a string numa matriz 4x4 (os 4
+primeiros caracteres devem ser a primeira linha da matriz). Imprima na tela quantas colunas da
+matriz possuem apenas caracteres numéricos (de 0 a 9).
+=end
 string = "1abc2fg52jln1256"
 string = string.split(//)
 def numeric?(lookAhead)
@@ -111,7 +120,9 @@ end
 
 puts "A matriz tem #{coluna_matriz} colunas com apenas caracteres numéricos."
 
-#04_1
+=begin 04_1 Leia um inteiro. Ele será a quantidade de degraus da escada. Imprima uma escada com
+essa quantidade de degraus. Exemplo: se o usuário digitar 4.
+=end
 puts "Digite um numero inteiro:"
 numero = gets.chomp.to_i
 
@@ -134,7 +145,11 @@ end
   puts "\n"
 end
 
-#01_2
+=begin 01_2 Faça um programa que pergunte a cada um dos clientes da academia seu código, sua
+altura e seu peso. O final da digitação de dados deve ser dado quando o usuário digitar 0 (zero) no
+campo código. Ao encerrar o programa, imprima os códigos e valores do cliente mais alto, do
+mais baixo, do mais gordo e do mais magro.
+=end
 cliente = {}
 
 puts "Digite o código:"
@@ -187,7 +202,10 @@ puts "O cliente mais alto foi #{cliente['baixo']}."
 puts "O cliente mais alto foi #{cliente['gordo']}."
 puts "O cliente mais alto foi #{cliente['magro']}."
 
-#02_2
+=begin 02_2 Leia uma string. O usuário somente digitará caracteres minúsculos. Verifique se todas
+as letras da string estão cercadas por números. Informe ao usuário o resultado. Ex: “h3-54b24”
+não está, por causa do ‘h’. Ex: “+23s48-9r3” está com todas as letras cercadas por números.
+=end
 puts "Digite uma palavra:"
 string = gets.chomp
 letra = string.split(//)
@@ -220,7 +238,10 @@ else
 end
 
 
-#03_2
+=begin 03_2 Leia uma string de 16 caracteres numéricos. Transforme a string numa matriz 4x4 de
+inteiros (os 4 primeiros caracteres devem ser a primeira linha da matriz). Calcule a soma de cada
+coluna e imprima somente a maior soma.
+=end
 puts "Digite um numeral de 16 caracteres:"
 numero = gets.chomp
 numero = numero.split(//)
@@ -258,7 +279,10 @@ end
 puts maiorSoma
 
 
-#04_2
+=begin 04_2 Leia 9 números e guarde-os numa matriz 3x3. Imprima: (soma da diagonal principal)
+menos a (soma da diagonal secundaria). Exemplo: na matriz abaixo, o resultado é: (1+5+9)-
+(3+5+7) = 15 – 15 = 0.
+=end
 puts "Digite 9 números:"
 numero = gets.chomp
 numero = numero.split(//)
