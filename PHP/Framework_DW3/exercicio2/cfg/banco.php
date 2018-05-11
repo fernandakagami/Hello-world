aa355bf13c54de0cd5f43935888e2379477e69a4
fernanda <fernandakagami@gmail.com>
2018-05-11 10:31:01 -0300

    Changing the database

--- PHP/Framework_DW3/exercicio2/cfg/banco.php
+++ PHP/Framework_DW3/exercicio2/cfg/banco.php
@@ -5,6 +5,6 @@ $banco = [
     'servidor' => 'localhost',
     'porta' => '3306',
     'banco' => 'exercicio2',
-    'usuario' => 'root',
-    'senha' => 'utfpr',
+    'usuario' => '###',
+    'senha' => '###',
 ];
